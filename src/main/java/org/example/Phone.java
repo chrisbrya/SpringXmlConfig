232f41065431e87a00aed7a513937896024091f6
Chrisbry <c.bryantcf@gmail.com>
2022-09-29 10:56:20 -0400

    XML Spring Config

---
+++ src/main/java/org/example/Phone.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Phone {
+
+    private String mob;
+
+    public String getMob() {
+        return mob;
+    }
+
+    public void setMob(String mob) {
+        this.mob = mob;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "mob='" + mob + '\'' +
+                '}';
+    }
+}
